4c0c7aa1fa62d2dc30fc62f2e9226fc8e4cb5dfe
Ram Thapa <thapa.ram9590@gmail.com>
2022-09-29 23:48:13 +1000

    Add password check program

--- Week2/password_stars.py
+++ Week2/password_stars.py
@@ -1,1 +1,7 @@
-print('Hello this is a test message')
+MIN_LENGTH = 8
+password = input("Please enter your password of minimum of 8 character: ")
+while len(password) < MIN_LENGTH:
+    password = input("Please enter your password of minimum of 8 character: ")
+
+print("*" * len(password))
+

61e3675ce537f5f1b0bf4ae2315096281994d33a
Ram Thapa <thapa.ram9590@gmail.com>
2022-10-04 22:38:43 +1000

    completed list_exercises.py as required

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -14,3 +14,11 @@ print(f"The first number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Please input your Username:")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access denied")

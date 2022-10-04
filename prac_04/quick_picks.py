173673e714ecf545575ff78adc71d48ee667e7be
Ram Thapa <thapa.ram9590@gmail.com>
2022-10-04 23:55:42 +1000

    Working on quick_picks.py

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,19 @@
+import random
+NUM_IN_LINES = 6
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+
+
+valid = False
+while not valid:
+    try:
+        quick_pick_number = int(input("How many quick picks? "))
+        valid = True
+    except ValueError:
+        print("Its not a number, Please input a proper number")
+for i in range(quick_pick_number):
+    quick_pick = []
+    for j in range(NUM_IN_LINES);
+    number = random.randint(MIN_NUMBER, MAX_NUMBER)
+
+

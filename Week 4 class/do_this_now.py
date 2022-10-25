9e395c7bebdd65ee685b0ac0aca60768bdec754b
Ram Thapa <thapa.ram9590@gmail.com>
2022-10-25 17:35:49 +1000

    update

--- Week 4 class/do_this_now.py
+++ Week 4 class/do_this_now.py
@@ -1,13 +1,14 @@
-names = ["Ada", "Alan", "Bill", "John"]
-print(" , ".join(names))
-name_to_remove = input("Who do you want to remove? ")
-
-while name_to_remove != "":
-    try:
-        names.remove(name_to_remove)
-        print(" , ".join(names))
-
-    except ValueError:
-        print("There is no such name.")
-    name_to_remove = input("Who do you want to remove? ")
-print("Thank you for deleting")
+# names = ["Ada", "Alan", "Bill", "John"]
+# print(" , ".join(names))
+# name_to_remove = input("Who do you want to remove? ")
+#
+# while name_to_remove != "":
+#     try:
+#         names.remove(name_to_remove)
+#         print(" , ".join(names))
+#
+#     except ValueError:
+#         print("There is no such name.")
+#     name_to_remove = input("Who do you want to remove? ")
+# print("Thank you for deleting")
+print("*".join([len(word) for word in "one*two*three".split('*')]))
